b57e1237312e98dae4afc50c97399159f3cd9e09
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-10-10 21:43:06 +0800

    Update embeddings.py

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -23,46 +23,52 @@ def calculate_similarity(embeddings):
     similarity_matrix = cosine_similarity(embeddings)
     return similarity_matrix
 
-def getvectorId(file_name_list,temp_file_path_output_list):
-    #Connect to your indexes
-    index_name = "chapiq-index"
-    index = pinecone.Index(index_name)
-    output = index.query(
-        top_k=367,
-        vector= [0] * 384, # embedding dimension
-        namespace='chapiq-namespace',
-        include_values=True # Your filters here
-    )
-    ids = [match['id'] for match in output['matches']]
-    print(ids)
-    return ids
+def insert_vectors_to_pinecone(vector_data_list, pinecone_index_name):
+    # Initialize Pinecone connection
+    index = pinecone.Index(index_name=pinecone_index_name)
 
-    #def getvectorId(file_name_list,temp_file_path_output_list):
-    #model = SentenceTransformer('all-MiniLM-L6-v2')
-    #contents = []
-    #tiktoken_encoding = tiktoken.get_encoding("gpt2")
-    #for index, temp_file_path_output in enumerate(temp_file_path_output_list):
-    #    loader = UnstructuredMarkdownLoader(temp_file_path_output)
-    #    doc = loader.load()
-    #    for text_chunk in doc:
-    #        content = text_chunk.page_content
-    #        token_count, tokens = get_token_count(content, "cl100k_base")
-    #        contents.append((text_chunk, content, token_count))
-    #    df = pd.DataFrame(contents, columns=['filename', 'file_content', 'tokens'])
-    #    df["vectorEmbed"] = df.file_content.apply(lambda x: model.encode(x).tolist()) 
-    #    df['id'] = [str(uuid4()) for _ in range(len(df))]
-    #    # Loop through each row in the DataFrame 'df'
-    #vector_data_list = []
-    #for _, row in df.iterrows():
-    #    vector_data = {
-    #        'id': row['id'],
-    #        'vectorEmbed': row['vectorEmbed'],  # Use the list directly, assuming 'vectorEmbed' is a list
-    #        'metadata': {'content': row['file_content'], 'file_name': row['filename'], 'weights': row['weights']}
-    #    }
-    #    vector_data_list.append(vector_data)
-    #    # Upsert the vector data into the Pinecone index
-    #upsert_response = index.upsert(vectors=vector_data_list, namespace='chapiq-namespace')
-    #return df, vector_data_list
+    # Upsert the vector data into the Pinecone index
+    upsert_response = index.upsert(vectors=vector_data_list)
+    return upsert_response
+
+def get_vector_data(temp_file_path_output_list):
+    # Initialize the SentenceTransformer model
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    # Create an empty list to store content and token count
+    contents = []
+
+    # Iterate through the document chunks
+    for temp_file_path_output in temp_file_path_output_list:
+        loader = UnstructuredMarkdownLoader(temp_file_path_output)
+        doc = loader.load()
+        
+        # Extract content and token count for each chunk
+        for text_chunk in doc:
+            content = text_chunk.page_content
+            token_count, _ = get_token_count(content, "cl100k_base")
+            contents.append((text_chunk, content, token_count))
+    
+    # Create a DataFrame from the contents
+    df = pd.DataFrame(contents, columns=['filename', 'file_content', 'tokens'])
+    
+    # Encode content using SentenceTransformer
+    df["vectorEmbed"] = df.file_content.apply(lambda x: model.encode(x).tolist())
+    
+    # Generate unique IDs for each row
+    df['id'] = [str(uuid4()) for _ in range(len(df))]
+    
+    # Create a list of dictionaries for vector data
+    vector_data_list = []
+    for _, row in df.iterrows():
+        vector_data = {
+            'id': row['id'],
+            'vectorEmbed': row['vectorEmbed'],
+            'metadata': {'content': row['file_content'], 'file_name': row['filename']}
+        }
+        vector_data_list.append(vector_data)
+    
+    return df, vector_data_list
 
 
 
